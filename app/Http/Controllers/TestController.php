65f1bea6e279505f8cf93f69bea5efac86eb70cc
Sayed Naweed <sayednaweedsayedy@gmail.com>
2025-01-21 10:47:36 +0430

    updated

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -22,24 +22,24 @@ class TestController extends Controller
     {
         $locale = "en";
 
-     $query =  DB::table('news as n')
-    ->join('news_trans as ntr', 'ntr.news_id', '=', 'n.id')
-    ->join('news_types as nt','nt.id','=','n.news_type_id')
-    ->join('news_type_trans as ntt','ntt.news_type_id','=','nt.id')
-    ->join('priorities as pri','pri.id','=','n.priority_id')
-    ->join('priority_trans as pt','pt.priority_id','=','pri.id')
-    ->join('users as us','us.id','=','n.user_id')
-    ->leftJoin('news_documents as nd','nd.news_id', '=', 'n.id')
-    ->where('ntr.language_name',$locale)
-    ->where('pt.language_name',$locale)
-    ->where('ntt.language_name',$locale)
-    ->select('ntr.contents')
-    ->get();
-    return $query;
+        $query =  DB::table('news as n')
+            ->join('news_trans as ntr', 'ntr.news_id', '=', 'n.id')
+            ->join('news_types as nt', 'nt.id', '=', 'n.news_type_id')
+            ->join('news_type_trans as ntt', 'ntt.news_type_id', '=', 'nt.id')
+            ->join('priorities as pri', 'pri.id', '=', 'n.priority_id')
+            ->join('priority_trans as pt', 'pt.priority_id', '=', 'pri.id')
+            ->join('users as us', 'us.id', '=', 'n.user_id')
+            ->leftJoin('news_documents as nd', 'nd.news_id', '=', 'n.id')
+            ->where('ntr.language_name', $locale)
+            ->where('pt.language_name', $locale)
+            ->where('ntt.language_name', $locale)
+            ->select('ntr.contents')
+            ->get();
+        return $query;
 
 
-dd($query->toSql(), $query->getBindings());
-    // ->get();
+        dd($query->toSql(), $query->getBindings());
+        // ->get();
 
         // ->join('')
         $query = DB::table('news AS n')
@@ -80,7 +80,7 @@ dd($query->toSql(), $query->getBindings());
             // Get the data
             ->get();
 
-            return $query;
+        return $query;
 
         // $query  = DB::table('news AS n')
         //     ->leftJoin('news_trans AS ntr', function ($join) use ($locale) {
