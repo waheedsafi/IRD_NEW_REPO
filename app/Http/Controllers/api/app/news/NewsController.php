e5bb426c36542479c3e41eaaee960894202189c1
Sayed Naweed <sayednaweedsayedy@gmail.com>
2025-01-22 08:48:31 +0430

    News updated

--- app/Http/Controllers/api/app/news/NewsController.php
+++ app/Http/Controllers/api/app/news/NewsController.php
@@ -10,25 +10,26 @@ use App\Models\NewsDocument;
 use App\Models\NewsTran;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\App;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 
 class NewsController extends Controller
 {
-    public function authNewses(Request $request)
+    public function authNewses(Request $request, $page)
     {
+        $perPage = $request->input('per_page', 10); // Number of records per page
+        $page = $request->input('page', 1); // Current page
         $locale = App::getLocale();
         $query =  DB::table('news as n')
             ->join('news_trans as ntr', 'ntr.news_id', '=', 'n.id')
             ->join('news_type_trans as ntt', 'ntt.news_type_id', '=', 'n.news_type_id')
             ->join('priority_trans as pt', 'pt.priority_id', '=', 'n.priority_id')
-            ->join('users as us', 'us.id', '=', 'n.user_id')
             ->leftJoin('news_documents as nd', 'nd.news_id', '=', 'n.id')
             ->where('ntr.language_name', $locale)
             ->where('pt.language_name', $locale)
             ->where('ntt.language_name', $locale)
+            ->where('n.visible', 1)
             ->select(
-                'n.id',
+                'n.id as id',
                 'n.visible',
                 'n.date',
                 'n.visibility_date',
@@ -36,13 +37,27 @@ class NewsController extends Controller
                 'ntt.value AS news_type',
                 'n.priority_id',
                 'pt.value AS priority',
-                'us.username AS user',
                 'ntr.title',
                 'ntr.contents',
-                'nd.url AS image'  // Assuming you want the first image URL
-            )
-            ->get();
-        return $query;
+                'nd.url AS image',  // Assuming you want the first image URL
+                'n.created_at'
+            );
+
+
+        $this->applyDate($query, $request);
+        $this->applyFilters($query, $request);
+        $this->applySearch($query, $request);
+
+        $result = $query->paginate($perPage, ['*'], 'page', $page);
+
+        return response()->json(
+            [
+                "newses" => $result,
+            ],
+            200,
+            [],
+            JSON_UNESCAPED_UNICODE
+        );
     }
 
     public function authNews(Request $request)
@@ -73,7 +88,6 @@ class NewsController extends Controller
             ->get();
         return $query;
     }
-
 
     public function publicNewses(Request $request)
     {
@@ -134,7 +148,6 @@ class NewsController extends Controller
             ->get();
         return $query;
     }
-
 
     public function store(NewsStoreRequest $request)
     {
@@ -216,5 +229,49 @@ class NewsController extends Controller
             [],
             JSON_UNESCAPED_UNICODE
         );
+    }
+    // date function 
+    protected function applyDate($query, $request)
+    {
+        // Apply date filtering conditionally if provided
+        $startDate = $request->input('filters.date.startDate');
+        $endDate = $request->input('filters.date.endDate');
+
+        if ($startDate) {
+            $query->where('n.created_at', '>=', $startDate);
+        }
+        if ($endDate) {
+            $query->where('n.created_at', '<=', $endDate);
+        }
+    }
+    // search function 
+    protected function applySearch($query, $request)
+    {
+
+
+        $searchColumn = $request->input('filters.search.column');
+        $searchValue = $request->input('filters.search.value');
+
+        if ($searchColumn && $searchValue) {
+            $allowedColumns = ['title', 'contents'];
+
+            // Ensure that the search column is allowed
+            if (in_array($searchColumn, $allowedColumns)) {
+                $query->where($searchColumn, 'like', '%' . $searchValue . '%');
+            }
+        }
+    }
+    // filter function
+    protected function applyFilters($query, $request)
+    {
+        $sort = $request->input('filters.sort'); // Sorting column
+        $order = $request->input('filters.order', 'asc'); // Sorting order (default 
+
+        if ($sort && in_array($sort, ['news_type_id', 'priority_id', 'visible', 'visibility_date', 'date'])) {
+            $query->orderBy($sort, $order);
+        } else {
+            // Default sorting if no sort is provided
+            $query->orderBy("created_at", 'desc');
+        }
     }
 }
--- routes/api/app/news/news.php
+++ routes/api/app/news/news.php
@@ -14,7 +14,7 @@ Route::prefix('v1')->group(function () {
   Route::get('/public/news/{id}', [NewsController::class, "publicNews"]);
 });
 Route::prefix('v1')->middleware(['api.key', "authorized:" . 'user:api'])->group(function () {
-  Route::get('/auth/newses', [NewsController::class, "authNewses"]);
-  Route::get('/auth/news/{id}', [NewsController::class, "authNews"]);
+  Route::get('/user/newses/{page}', [NewsController::class, "authNewses"]);
+  Route::get('/user/news/{id}', [NewsController::class, "authNews"]);
   Route::post('news/store', [NewsController::class, 'store']);
 });
