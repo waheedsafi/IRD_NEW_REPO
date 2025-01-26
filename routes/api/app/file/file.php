66d36c238661f782ed241cdd3e2e15e5642b8f21
Sayed Naweed <sayednaweedsayedy@gmail.com>
2025-01-26 11:43:25 +0430

    indentation fixed

--- routes/api/app/file/file.php
+++ routes/api/app/file/file.php
@@ -8,18 +8,11 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::prefix('v1')->middleware(['api.key', "authorized:" . 'user:api'])->group(function () {
-
-  
-
-  Route::post('news/file/upload',[FileController::class,'newsFileUpload']);
-
-
+  Route::post('news/file/upload', [FileController::class, 'newsFileUpload']);
 });
 
 Route::prefix('v1')->middleware(['api.key', "authorized:" . 'user:api'])->group(function () {
 
 
   Route::post('file/upload', [FileController::class, 'fileUpload']);
-  
-
 });
