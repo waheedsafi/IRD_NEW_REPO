03faf67d874efab66f04e4df9ded1ade937b5cf8
Sayed Naweed <sayednaweedsayedy@gmail.com>
2025-01-28 15:21:55 +0430

    countries function update

--- routes/api/template/location.php
+++ routes/api/template/location.php
@@ -4,8 +4,8 @@
 use App\Http\Controllers\api\template\LocationController;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('v1')->middleware(['api.key', "authorized:" . 'user:api'])->group(function () {
-    Route::get('/contries', [LocationController::class, "contries"]);
+Route::prefix('v1')->middleware(['api.key'])->group(function () {
+    Route::get('/countries', [LocationController::class, "countries"]);
     Route::get('/provinces', [LocationController::class, "provinces"]);
     Route::get('/districts', [LocationController::class, 'districts']);
 });
