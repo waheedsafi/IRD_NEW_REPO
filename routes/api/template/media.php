d656125e6a057d15a77d931a222a6daa2e0a8998
Sayed Naweed <sayednaweedsayedy@gmail.com>
2025-02-11 10:05:11 +0430

    media controller updated

--- routes/api/template/media.php
+++ routes/api/template/media.php
@@ -5,8 +5,8 @@ use App\Http\Controllers\api\template\MediaController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('v1')->middleware(['api.key', "authorized:" . 'user:api'])->group(function () {
-    Route::get('/media/{storage}/{folder}/{filename}', [MediaController::class, "show"]);
-    Route::get('/media/{storage}/{access}/{folder}/{filename}', [MediaController::class, "showPublic"]);
-    Route::get('/media/{storage}/{filename}', [MediaController::class, "downloadTemp"]);
-    Route::get('/media/{storage}/{folder}/{folderType}/{filename}', [MediaController::class, "downloadDoc"]);
+    // Route::get('/media/{storage}/{folder}/{filename}', [MediaController::class, "show"]);
+    // Route::get('/media/{storage}/{access}/{folder}/{filename}', [MediaController::class, "showPublic"]);
+    Route::get('/media', [MediaController::class, "downloadFile"]);
+    // Route::get('/media/{storage}/{folder}/{folderType}/{filename}', [MediaController::class, "downloadDoc"]);
 });
