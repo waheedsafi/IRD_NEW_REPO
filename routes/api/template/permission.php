67f454ce92269aed3d42566a795aeed3ab490033
waheed <wahidsafi.198@gmail.com>
2025-02-15 10:24:50 +0430

    sub_permission

--- routes/api/template/permission.php
+++ routes/api/template/permission.php
@@ -4,6 +4,10 @@
 use App\Http\Controllers\api\template\PermissionController;
 use Illuminate\Support\Facades\Route;
 
+
+Route::get('sub-permissions', [PermissionController::class, 'subPermissions']);
+
 Route::prefix('v1')->middleware(['api.key', "authorized:" . 'user:api'])->group(function () {
     Route::get('/permissions/{id}', [PermissionController::class, "permissions"]);
+    Route::get('sub-permissions', [PermissionController::class, 'subPermissions']);
 });
