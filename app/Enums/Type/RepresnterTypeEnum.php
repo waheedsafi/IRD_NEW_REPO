3018111cfc79956300c50b5b3d881aa9553e6208
waheed <wahidsafi.198@gmail.com>
2025-02-15 14:47:51 +0430

    permission update

---
+++ app/Enums/Type/RepresnterTypeEnum.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Enums\Type;
+
+enum RepresnterTypeEnum: int
+{
+  case ngo = 1;
+  case project = 2;
+}
--- routes/api/template/permission.php
+++ routes/api/template/permission.php
@@ -10,4 +10,5 @@ Route::get('sub-permissions', [PermissionController::class, 'subPermissions']);
 Route::prefix('v1')->middleware(['api.key', "authorized:" . 'user:api'])->group(function () {
     Route::get('/permissions/{id}', [PermissionController::class, "permissions"]);
     Route::get('sub-permissions', [PermissionController::class, 'subPermissions']);
+    Route::post('sub-permission/update', [PermissionController::class,'userPermissionUpdate']);
 });
