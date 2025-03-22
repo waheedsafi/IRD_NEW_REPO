2386823dfcf1578491410c9a3eedc1f41a25e938
waheedsafi <wahidsafi.198@gmail.com>
2025-03-22 12:54:48 +0430

    audit filter

--- routes/api/template/audit.php
+++ routes/api/template/audit.php
@@ -7,13 +7,15 @@ use Illuminate\Support\Facades\Route;
 
 
 
-Route::get('/audits/user/type', [AuditLogController::class, "userType"]);
-Route::get('/audits/user/list', [AuditLogController::class, "userList"]);
-Route::get('/audits/table/list', [AuditLogController::class, "tableList"]);
-Route::get('/audits/column/list', [AuditLogController::class, "columnList"]);
 
 Route::get('/audits/{page}', [AuditLogController::class, "audits"]);
 
 Route::prefix('v1')->middleware(['api.key', "authorized:" . 'user:api'])->group(function () {
     Route::get('/audits/{page}', [AuditLogController::class, "audits"])->middleware(["hasViewPermission:" . PermissionEnum::audit->value]);
+
+
+    Route::get('/audits/user/type', [AuditLogController::class, "userType"]);
+    Route::get('/audits/user/list', [AuditLogController::class, "userList"]);
+    Route::get('/audits/table/list', [AuditLogController::class, "tableList"]);
+    Route::get('/audits/column/list', [AuditLogController::class, "columnList"]);
 });
