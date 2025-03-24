ed157b30c69f8e3fcc5d6805907ba361795941a2
waheedsafi <wahidsafi.198@gmail.com>
2025-03-24 22:13:27 +0430

    user login log route , controller and function

--- routes/api/template/log.php
+++ routes/api/template/log.php
@@ -7,10 +7,12 @@ use Illuminate\Support\Facades\Route;
 
 
 
-Route::get('/user/activities', [UserLoginLogController::class, "logs"]);
 
 Route::prefix('v1')->middleware("authorized:" . 'user:api')->group(function () {
     Route::get('/file-logs', [LogController::class, "fileLogs"]);
     Route::get('/database-logs', [LogController::class, "databaseLogs"]);
     Route::post('/logs/clear', [LogController::class, "clear"]);
+
+
+    Route::get('/user/activities', [UserLoginLogController::class, "logs"]);
 });
